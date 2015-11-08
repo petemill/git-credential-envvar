783ee1992060aa55c25df3c0e15af69febbe05b7
petemill <miller.pete+commit@gmail.com>
2015-11-08 14:09:59 -0800

    turns out we do not need to repeat back the host / protocol / path back to git, as git just uses any info we provide to replace what it already knows. simpler just to provide the credentials we know, and let it keep it's knowledge about the Url.

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ module.exports = function() {
   if (inCommand.toLowerCase() !== 'get') {
     process.exit();
   }
-
   process.stdin.pipe(require('split')()).on('data', processLine);
   let hardcodedUsername = process.env.GITCREDENTIALUSERNAME;
   let hardcodedPassword = process.env.GITCREDENTIALPASSWORD;
@@ -15,39 +14,11 @@ module.exports = function() {
     console.error('Could not find required environment variable GITCREDENTIALUSERNAME and/or GITCREDENTIALPASSWORD');
     process.exit(1);
   }
-
-  let inHost, inProtocol, inPath;
-
   function processLine (line) {
 
     //when line has content, it will be of the form key=value
-    if (line) {
-      let lineComponents = line.split('=');
-      let key = lineComponents[0];
-      let value = lineComponents[1];
-      switch (key) {
-        case 'host':
-          inHost = value;
-          break;
-        case 'protocol':
-          inProtocol = value;
-          break;
-        case 'path':
-          inPath = value;
-          break;
-        default:
-          console.error(`key: '${key}' not supported`);
-          process.exit(1);
-      }
-    }
     //when line has no content, we are to print the output
-    else {
-      if (inProtocol)
-        console.log(`protocol=${inProtocol}`);
-      if (inHost)
-        console.log(`host=${inHost}`);
-      if (inPath)
-        console.log(`path=${inPath}`);
+    if (!line) {
       console.log(`username=${hardcodedUsername}`);
       console.log(`password=${hardcodedPassword}`);
       process.exit();
